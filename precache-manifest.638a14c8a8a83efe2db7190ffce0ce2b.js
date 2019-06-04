1e1acad9f8b640617b3015c6c83b706efff3bc52
Giuliano Machado <giufma@gmail.com>
2019-06-04 18:08:29 -0300

    Updates

---
+++ precache-manifest.638a14c8a8a83efe2db7190ffce0ce2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1901a597a14281d2538bb9b01061b1d",
+    "url": "/random-quotes-generator/index.html"
+  },
+  {
+    "revision": "becef483a1a81558e9d9",
+    "url": "/random-quotes-generator/static/css/main.e3446c18.chunk.css"
+  },
+  {
+    "revision": "0f1c5798ca0a25facf8c",
+    "url": "/random-quotes-generator/static/js/2.d5ed7b26.chunk.js"
+  },
+  {
+    "revision": "becef483a1a81558e9d9",
+    "url": "/random-quotes-generator/static/js/main.f877fc18.chunk.js"
+  },
+  {
+    "revision": "8e6072241771d5b21e9d",
+    "url": "/random-quotes-generator/static/js/runtime~main.416aac4e.js"
+  }
+]);
